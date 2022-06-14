888c6f1da860e444718d86c483ff3628966955ee
daanyalmcorpsains <92033071+daanyalmcorpsains@users.noreply.github.com>
2022-06-14 10:35:16 +0100

    Revert "Update consumer.py"

--- faust/transport/consumer.py
+++ faust/transport/consumer.py
@@ -1332,7 +1332,7 @@ class ConsumerThread(QueueServiceThread):
     ) -> None:
         """Call on rebalance when partitions are being assigned."""
         await self.consumer.threadsafe_partitions_assigned(
-            self.parent_loop, assigned, generation_id
+            self.thread_loop, assigned, generation_id
         )
 
     @abc.abstractmethod
