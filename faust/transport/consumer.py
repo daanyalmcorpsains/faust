a716ce3094ea17c6b65293fa311f0404afa7c86b
daanyalmcorpsains <92033071+daanyalmcorpsains@users.noreply.github.com>
2022-06-14 10:43:38 +0100

    Update consumer.py

--- faust/transport/consumer.py
+++ faust/transport/consumer.py
@@ -1051,7 +1051,7 @@ class Consumer(Service, ConsumerT):
                     
                 # Sleeping because sometimes getmany is called in a loop
                 # never releasing to the event loop
-                await sleep(0)
+                await self.sleep(0)
                 if not self.should_stop:
                     async for tp, message in ait:
                         self.log.info(f'the topic partiion is {tp} and the offset is {message.offset}')
