72c4dd87f5bf8992345e01ecd604fe4ae12220cc
daanyalmcorpsains <92033071+daanyalmcorpsains@users.noreply.github.com>
2022-06-16 15:56:53 +0100

    Revert "Update consumer.py"

--- faust/transport/drivers/confluent.py
+++ faust/transport/drivers/confluent.py
@@ -420,7 +420,7 @@ class ConfluentConsumerThread(ConsumerThread, BrokerCredentialsMixin):
         _consumer = self._ensure_consumer()
         messages = await self.call_thread(
             _consumer.consume,
-            num_messages=500000,
+            num_messages=1000,
             timeout=timeout,
         )        
         
