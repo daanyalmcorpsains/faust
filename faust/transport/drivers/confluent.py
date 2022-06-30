e6417e4fc71b15ace6f9a9d3907accafc0c59cc5
daanyalmcorpsains <92033071+daanyalmcorpsains@users.noreply.github.com>
2022-06-30 17:52:15 +0100

    Update confluent.py

--- faust/transport/drivers/confluent.py
+++ faust/transport/drivers/confluent.py
@@ -436,7 +436,7 @@ class ConfluentConsumerThread(ConsumerThread, BrokerCredentialsMixin):
         _consumer = self._ensure_consumer()
         messages = await self.call_thread(
             _consumer.consume,
-            num_messages=1001,
+            num_messages=301,
             timeout=timeout,
         )        
         
